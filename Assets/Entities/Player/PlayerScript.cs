9268c5dac2be34be2e3f43c48f088a5755f9bd23
Damien Mayance <valryon@gmail.com>
2013-02-11 21:25:54 +0100

    Firing bullet

--- Assets/Entities/Player/PlayerScript.cs
+++ Assets/Entities/Player/PlayerScript.cs
@@ -7,26 +7,46 @@ public class PlayerScript : MonoBehaviour
 	/// <summary>
 	/// Crosshairs
 	/// </summary>
-	public Transform Crosshair1, CrossHair2;
+	public Transform Crosshair1;
+	public GameObject Bullet;
+	public float FireCooldown;
+	private float currentFireCooldown;
 	
 	void Start ()
 	{
 		// Hide mouse :3
 		Screen.showCursor = false;
+		
+		currentFireCooldown = FireCooldown;
 	}
 	
 	void Update ()
 	{
+		currentFireCooldown -= Time.deltaTime;
+		
 		if (Crosshair1 != null) {
 			
 			RaycastHit hit;
 			Ray ray = Camera.mainCamera.ScreenPointToRay (Input.mousePosition);
 			
 			if (Physics.Raycast (ray, out hit) && hit.transform.name == "Ground") {
-				Crosshair1.localPosition = new Vector3(hit.point.x, 2 , hit.point.z);
+				Crosshair1.localPosition = new Vector3 (hit.point.x, 2, hit.point.z);
 			}
 			
+			if (currentFireCooldown <= 0) {
+				currentFireCooldown = FireCooldown;	
 				
+				// Instanciate a bullet
+				if (Bullet != null) {
+					
+					GameObject b = (GameObject)GameObject.Instantiate (Bullet);
+					
+					BulletScript bulletScript = (BulletScript) b.GetComponent("BulletScript");
+					
+					bulletScript.Direction = (Crosshair1.localPosition - this.transform.localPosition);
+					bulletScript.Direction.Normalize();
+				}
+			}
 			
 		}
 	}
